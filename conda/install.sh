44cf7667637a19155c607d759cb1b01c8f8158c3
Masataro Asai <guicho2.71828@gmail.com>
2025-09-12 11:51:05 -0400

    feat: getopts for -h option

--- conda/install.sh
+++ conda/install.sh
@@ -22,6 +22,31 @@ else
 fi
 
 
+usage(){
+    echo "Usage: install.sh [-h]"
+    echo
+    echo "-h : show this help"
+    exit 1
+}
+
+while getopts "h" OPTNAME ; do
+    case "${OPTNAME}" in
+        h)
+            usage
+            ;;
+        :)
+            # If expected argument omitted:
+            echo "Error: -${OPTARG} requires an argument."
+            exit 1
+            ;;
+        *)
+            # If unknown (any other) option:
+            echo "Error: -${OPTARG} unknown."
+            exit 1
+            ;;
+    esac
+done
+
 # note:
 # this is a portable way (works in linux and osx) to get the directory of this script.
 # readlink -ef may not work on osx.

39bf127f8c6b07081ac949d975de491d1a7e6f3e
Avinash Balakrishnan <avinash2692@gmail.com>
2026-03-27 08:54:05 -0700

    fix: setting ollama host in conftest (#751)
    * small fix to point conftest to ollama port if running
    
    * getting port also from env

--- test/conftest.py
+++ test/conftest.py
@@ -37,11 +37,17 @@ def _check_ollama_available():
     """
     import socket
 
+    host_str = os.environ.get("OLLAMA_HOST", "127.0.0.1:11434")
+    # OLLAMA_HOST may be "host:port" or just "host" (bare IP without port)
+    if ":" in host_str:
+        host, port = host_str.rsplit(":", 1)
+        port = int(port)
+    else:
+        host, port = host_str, int(os.environ.get("OLLAMA_PORT", 11434))
     try:
-        # Try to connect to Ollama's default port
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
-        result = sock.connect_ex(("localhost", 11434))
+        result = sock.connect_ex((host, port))
         sock.close()
         return result == 0
     except Exception:
